117c068091a4e79973ff06972db01a6023513366
Evgenii Anchugov <bergy@yandex.ru>
2024-05-25 23:27:18 +0300

    init

--- src/main/java/com/example/test/controller/testController.java
+++ src/main/java/com/example/test/controller/testController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @GetMapping
     public String hi(){
-        return "hello darling";
+        return "hello darling!";
     }
 }
