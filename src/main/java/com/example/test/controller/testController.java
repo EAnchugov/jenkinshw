5117b8647fc3b282a874a0db068b4d3cf85138a4
Evgenii Anchugov <bergy@yandex.ru>
2024-05-28 17:24:54 +0300

    make war not jar

--- src/main/java/com/example/test/controller/testController.java
+++ src/main/java/com/example/test/controller/testController.java
@@ -1,11 +1,13 @@
 package com.example.test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test/")
 public class testController {
-    @GetMapping
+    @GetMapping()
     public String hi(){
         return "hello darling++---+++";
     }
